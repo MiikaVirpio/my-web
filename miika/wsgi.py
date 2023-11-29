8d3517681ce0516d33a011078e48d5591369c61c
bqnbebw <bqnbebw@ssh01.cluster030.gra.hosting.ovh.net>
2023-11-29 16:58:44 +0000

    commit from server ssh

---
+++ miika/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for miika project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+this_file = "venv/bin/activate_this.py"
+exec(open(this_file).read(), {'__file__': this_file})
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miika.settings')
+
+application = get_wsgi_application()
