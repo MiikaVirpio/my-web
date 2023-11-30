c806989b2692cf0d4fbc6549066df1fb959164ee
Miika Virpiö <virpiomi@arcada.fi>
2023-11-30 03:07:57 +0200

    static root pre

--- miika/settings.py
+++ miika/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'miika', 'static'),]
 
+STATIC_ROOT = "/home/bqnbebw/www/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
